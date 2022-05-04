7836c4c5b61f1069b9ccb44ec19c0f76cddedab7
BajcarM <102542768+BajcarM@users.noreply.github.com>
2022-05-04 18:09:06 +0200

    added functions for corner vectors, angle

--- script.js
+++ script.js
@@ -3,27 +3,28 @@ const plane = document.querySelector(".plane");
 let tilesArray = [];
 let tileSize = 200;
 
-tilesArray.push({
-  id: 0,
+for (i = 0; i < 3; i++) {
+  tilesArray.push({
+    id: i,
 
-  coordsTopL: null,
-  coordsTopR: null,
-  coordsBotL: null,
-  coordsBotR: null,
-  coordsCenter: null,
+    cornerTopL: { x: null, y: null },
+    cornerTopR: { x: null, y: null },
+    cornerBotL: { x: null, y: null },
+    cornerBotR: { x: null, y: null },
+    coordsCenter: { x: null, y: null },
 
-  vectorVisibleAngleU: null,
-  vectorVisibleAngleV: null,
+    vectorU: { x: null, y: null },
+    vectorV: { x: null, y: null },
 
-  visible: true,
-  opacity: null,
+    visible: true,
+    opacity: null,
 
-  player: false,
-  wall: false,
-});
-// tilesArray.push({
-//   id: 1,
-// });
+    player: false,
+    wall: false,
+  });
+}
+
+let tilePlayer = tilesArray[0];
 
 function displayTiles() {
   const preparedTiles = tilesArray.map((tile) => {
@@ -33,28 +34,102 @@ function displayTiles() {
   plane.innerHTML = preparedTiles.join("");
 }
 
-displayTiles();
-
 function getOwnCoords() {
   tilesArray.forEach((tile) => {
     let tileRect = document
       .querySelector(`[data-id="${tile.id}"]`)
       .getBoundingClientRect();
 
-    console.log(tileRect);
-
-    tile.coordsTopL = [tileRect.left, tileRect.top];
-    tile.coordsTopR = [tileRect.right, tileRect.top];
-    tile.coordsBotL = [tileRect.left, tileRect.bottom];
-    tile.coordsBotR = [tileRect.right, tileRect.bottom];
-    tile.coordsCenter = [
-      tileRect.left + tileRect.width / 2,
-      tileRect.top + tileRect.height / 2,
-    ];
+    tile.cornerTopL = { x: tileRect.left, y: tileRect.top };
+    tile.cornerTopR = { x: tileRect.right, y: tileRect.top };
+    tile.cornerBotL = { x: tileRect.left, y: tileRect.bottom };
+    tile.cornerBotR = { x: tileRect.right, y: tileRect.bottom };
+    tile.coordsCenter = {
+      x: tileRect.left + tileRect.width / 2,
+      y: tileRect.top + tileRect.height / 2,
+    };
   });
 }
 
+function getVisibleBorderVectors(tile) {
+  let vectorCorners = getVectorCorners();
 
+  tile.vectorU = {
+    x: vectorCorners[0].x - tilePlayer.coordsCenter.x,
+    y: vectorCorners[0].y - tilePlayer.coordsCenter.y,
+  };
 
+  tile.vectorV = {
+    x: vectorCorners[1].x - tilePlayer.coordsCenter.x,
+    y: vectorCorners[1].y - tilePlayer.coordsCenter.y,
+  };
+
+  function getVectorCorners() {
+    let tileCorners = [
+      tile.cornerTopL,
+      tile.cornerTopR,
+      tile.cornerBotL,
+      tile.cornerBotR,
+    ];
+
+    tileCorners = tileCorners.map((corner) => {
+      return {
+        x: corner.x,
+        y: corner.y,
+        distanceSquare: distanceSquare(corner, tilePlayer.coordsCenter),
+      };
+    });
+
+    tileCorners.sort((a, b) => {
+      return a.distanceSquare - b.distanceSquare;
+    });
+
+    let vectorCorners =
+      tileCorners[0].distanceSquare === tileCorners[1].distanceSquare
+        ? tileCorners.slice(0, 2)
+        : tileCorners.slice(1, 3);
+
+    return vectorCorners;
+  }
+}
+
+function distanceSquare(a, b) {
+  return (b.x - a.x) * (b.x - a.x) + (b.y - a.y) * (b.y - a.y);
+}
+
+function crossProduct(u, v) {
+  return u.x * v.y - u.y * v.x;
+}
+
+function dotProduct(u, v) {
+  return u.x * v.x + u.y * v.y;
+}
+
+function angle(u, v) {
+  Math.atan2(Math.abs(crossProduct(u, v)), dotProduct(u, v));
+}
+
+function decideTileVisible(tile, tileWall) {
+  let tileCenterVector = {
+    x: tile.coordsCenter.x - tilePlayer.coordsCenter.x,
+    y: tile.coordsCenter.y - tilePlayer.coordsCenter.y,
+  };
+  let wallVisionAngle = angle(tileWall.vectorU, tileWall.vectorV);
+  let tileCenterAngle = angle(tileWall.vectorU, tileCenterVector);
+
+  if (
+    (crossProduct(tileWall.vectorU, tileWall.vectorV) > 0 &&
+      tileCenterAngle > 0 &&
+      tileCenterAngle < wallVisionAngle) ||
+    (crossProduct(tileWall.vectorU, tileWall.vectorV) < 0 &&
+      tileCenterAngle < 0 &&
+      tileCenterAngle > wallVisionAngle)
+  ) {
+    tile.visible = false;
+  }
+}
+
+displayTiles();
 getOwnCoords();
-console.log(tilesArray[0]);
+getVisibleBorderVectors(tilesArray[1]);
+angle(tilesArray[1].vectorU, tilesArray[1].vectorV);
